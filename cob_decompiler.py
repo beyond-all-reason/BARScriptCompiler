68afa91405d4ea74e6432b8bb7e63f0658d8a27e
Beherith <mysterme@gmail.com>
2024-04-19 23:49:58 +0200

    a wild decomplier appears

---
+++ cob_decompiler.py
@@ -0,0 +1,129 @@
+#import bos2cob_py3
+import cob_file
+import struct
+
+pf = cob_file.PACK_FORMAT
+hf = cob_file.COB_HEADER_FIELDS
+
+
+OPCODES = {
+	'MOVE'        : 0x10001000,
+	'TURN'        : 0x10002000,
+	'SPIN'        : 0x10003000,
+	'STOP_SPIN'   : 0x10004000,
+	'SHOW'        : 0x10005000,
+	'HIDE'        : 0x10006000,
+	'CACHE'       : 0x10007000,
+	'DONT_CACHE'  : 0x10008000,
+	'MOVE_NOW'    : 0x1000B000,
+	'TURN_NOW'    : 0x1000C000,
+	'SHADE'       : 0x1000D000,
+	'DONT_SHADE'  : 0x1000E000,
+	'DONT_SHADOW' : 0x1000E000,
+	'EMIT_SFX'    : 0x1000F000,
+
+	'WAIT_FOR_TURN'  : 0x10011000,
+	'WAIT_FOR_MOVE'  : 0x10012000,
+	'SLEEP'          : 0x10013000,
+
+	'PUSH_CONSTANT'    : 0x10021001,
+	'PUSH_LOCAL_VAR'   : 0x10021002,
+	'PUSH_STATIC'      : 0x10021004,
+	'CREATE_LOCAL_VAR' : 0x10022000,
+	'POP_LOCAL_VAR'    : 0x10023002,
+	'POP_STATIC'       : 0x10023004,
+	'POP_STACK'        : 0x10024000,
+
+	'ADD'         : 0x10031000,
+	'SUB'         : 0x10032000,
+	'MUL'         : 0x10033000,
+	'DIV'         : 0x10034000,
+	'MOD'         : 0x10034001,
+	'BITWISE_AND' : 0x10035000,
+	'BITWISE_OR'  : 0x10036000,
+	'BITWISE_XOR' : 0x10037000,
+	'BITWISE_NOT' : 0x10038000,
+
+	'RAND'           : 0x10041000,
+	'GET_UNIT_VALUE' : 0x10042000,
+	'GET'            : 0x10043000,
+
+	'SET_LESS'             : 0x10051000,
+	'SET_LESS_OR_EQUAL'    : 0x10052000,
+	'SET_GREATER'          : 0x10053000,
+	'SET_GREATER_OR_EQUAL' : 0x10054000,
+	'SET_EQUAL'            : 0x10055000,
+	'SET_NOT_EQUAL'        : 0x10056000,
+	'LOGICAL_AND'          : 0x10057000,
+	'LOGICAL_OR'           : 0x10058000,
+	'LOGICAL_XOR'          : 0x10059000,
+	'LOGICAL_NOT'          : 0x1005A000,
+
+	'START_SCRIPT'    : 0x10061000,
+	'CALL_SCRIPT'     : 0x10062000,
+	'REAL_CALL'       : 0x10062001,
+	'LUA_CALL'        : 0x10062002,
+	'JUMP'            : 0x10064000,
+	'RETURN'          : 0x10065000,
+	'JUMP_NOT_EQUAL'  : 0x10066000,
+	'SIGNAL'          : 0x10067000,
+	'SET_SIGNAL_MASK' : 0x10068000,
+
+	'EXPLODE'    : 0x10071000,
+	'PLAY_SOUND' : 0x10072000,
+
+	'SET'         : 0x10082000,
+	'ATTACH_UNIT' : 0x10083000,
+	'DROP_UNIT'   : 0x10084000,
+}
+rop = {}
+for k,v in OPCODES.items():
+    rop[v] = k
+
+def get_name(cob, pos):
+    start = pos
+    res = b''
+    while cob[pos]!= 0:
+        #print(cob[pos])
+        #res += int.to_bytes(cob[pos])
+        pos +=1
+    return cob[start:pos]
+
+def get_uint(cob, pos):
+    return struct.unpack(pf%1, cob[pos:pos+4])[0]
+
+def decompile(fname):
+    cobf = open(fname,'rb').read()
+    offset = 0
+    header = struct.unpack(cob_file.PACK_FORMAT%(len(cob_file.COB_HEADER_FIELDS)), cobf[0:4*len(hf)])
+    print (header)
+    hd = {}
+    for i in zip(hf, header):
+        print (i)
+        hd[i[0]] = i[1]
+
+    NumberOfPieces = hd["NumberOfPieces"]
+    OffsetToPieceNameOffsetArray = hd['OffsetToPieceNameOffsetArray']
+    pieces = []
+    for i in range(NumberOfPieces):
+        offset = get_uint(cobf, OffsetToPieceNameOffsetArray + 4*i)
+        pieces.append(get_name(cobf,offset))
+    print (pieces)
+    scripts = [get_name(cobf, get_uint(cobf, hd['OffsetToScriptNameOffsetArray'] + 4*i)) for i in range (hd["NumberOfScripts"])]
+    print (scripts)
+    scriptcode = [get_uint(cobf, 4*len(hf) + 4*i) for i in range(hd["TotalScriptLen"])]
+    scriptoffsets = [get_uint(cobf, hd["OffsetToScriptCodeIndexArray"] + 4*i) for i in range(hd["NumberOfScripts"])]
+    print(scriptoffsets)
+
+    cmd = ''
+    for i, op in enumerate(scriptcode[0:599]):
+        if op in rop:
+            print(cmd)
+            cmd = '%6x %s'%(i, rop[op])
+        
+            #print (op,rop[op])
+        else:
+            cmd += ' ' + str(op)
+
+
+decompile("units/armcrus.cob")
