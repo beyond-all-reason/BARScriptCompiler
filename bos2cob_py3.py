2d77bf05570605545ad72c4282b55ea7de791023
TheSilverHornet <37483246+TheSilverHornet@users.noreply.github.com>
2024-06-12 13:55:34 +0100

    Updated bos2cob_py3.py error logging
    Seperated and added output labelling to the components of the error logging

--- bos2cob_py3.py
+++ bos2cob_py3.py
@@ -1452,7 +1452,9 @@ if not args.nopcpp:
 			return self.output.getvalue()
 
 		def on_error(self, file, line, msg):
-			print(file, line, msg)
+			print("File: " + file)
+			print("Line number: " + str(line))
+			print("Error: " + msg)
 			sys.exit(1)
 			return super().on_error(file, line, msg)()
 
